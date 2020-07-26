3e092669e7d7c99b65e5934e9a75fb01ef750737
krnb <karan.01b@gmail.com>
2020-07-26 14:55:30 +0530

    bof test

---
+++ bof/connect.py
@@ -0,0 +1,20 @@
+import socket
+import sys
+
+rhost = "192.168."
+rport = 110
+
+
+try:
+	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	s.connect((rhost,rport))
+	print s.recv(1024)
+	s.send('USER test\r\n')
+	print s.recv(1024)
+	s.send('PASS asdf\r\n')
+	print s.recv(1024)
+	s.send('QUIT\r\n')
+	s.close()
+except:
+	print "Oops! Something went wrong!"
+	sys.exit()
